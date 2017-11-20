00acf14d9632d94f7d91633a1c2b2dab8417df7e
jss7801 <jss2981@naver.com>
2017-11-20 19:55:02 +0900

    mj

---
+++ draw/draw.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include <windows.h>
+#include <conio.h>
+#include <time.h>
+#include <string.h>
+
+void draw(int sx, int sy, int x, int y){
+int tmp;
+gotoxy(sx, sy);
+
+for(tmp=0; tmp<x; tmp++){
+printf("=");
+}
+
+for(tmp=0; tmp<y; tmp++){
+gotoxy(sx-1, (sy+1)+tmp);
+printf("|\n");
+}
+
+for(tmp=0; tmp<y; tmp++){
+gotoxy(sx+x,(sy+1)+tmp);
+printf("|\n");
+}
+
+gotoxy(sx, (sy+1)+y);
+for(tmp=0; tmp<x; tmp++){
+printf("=");
+}
+}
